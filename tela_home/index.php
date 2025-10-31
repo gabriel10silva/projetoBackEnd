09cbe7bbd0a6c28918b051582545193a9f5c84a8
gabriel10silva <gabrielsilva125023a@gmail.com>
2025-10-30 21:49:41 -0300

    ajuste favicon

--- tela_home/index.php
+++ tela_home/index.php
@@ -45,8 +45,8 @@ if ($user) {
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>StackShare</title>
-  <link rel="stylesheet" href="style.css">
   <link rel="shortcut icon" href="../uploads/img_favicon/favicon.ico" type="image/x-icon">
+  <link rel="stylesheet" href="style.css">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css" />
   <link rel="stylesheet" href="https://unpkg.com/boxicons@2.0.7/css/boxicons.min.css" />
 </head>
